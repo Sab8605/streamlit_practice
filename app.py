ddf87b051ef616fd04879e5b0d19803f4363ce3b
Sab8605 <91776637+Sab8605@users.noreply.github.com>
2022-11-13 18:12:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,81 +1,89 @@
-try:
-    import detectron2
-except:
-    import os 
-    os.system('pip install git+https://github.com/facebookresearch/detectron2.git')
+from detectron2.engine import DefaultPredictor
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+from detectron2.utils.video_visualizer import VideoVisualizer
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.utils.visualizer import ColorMode, Visualizer
+from detectron2 import model_zoo
+from PIL import Image
+import PIL
+import cv2
+import numpy as np
+import tqdm
+
+
+
+class Detector:
+
+    def __init__(self, model_type = "instanceSegmentation"):
+        self.cfg = get_cfg()
+        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+        self.cfg.MODEL.WEIGHTS = "model_final_instance_segmentation.pth"
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+        self.cfg.MODEL.DEVICE = "cpu" # cpu or cuda
+
+        self.predictor = DefaultPredictor(self.cfg)
+
+    def onImage(self, imagePath):
+        image = cv2.imread(imagePath)
+        predictions = self.predictor(image)
+
+        viz = Visualizer(image[:,:,::-1],metadata= MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]),scale=0.5)#instance_mode=ColorMode.IMAGE_BW)
+
+        output = viz.draw_instance_predictions(predictions['instances'].to('cpu'))
+        filename = 'result.jpg'
+        cv2.imwrite(filename, output.get_image()[:,:,::-1])
+        cv2.imshow("Result",output.get_image()[:,:,::-1])
+
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+
+
 
 import streamlit as st
 
+#detector = Detector(model_type='keypointsDetection')
 
-
-import numpy as np
-
-# import cv2
-# import requests
-import torch
-from PIL import Image
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog
-from detectron2.utils.visualizer import ColorMode
-import os
-
-
-def load_image(image_file):
-	img = Image.open(image_file)
-	return img
-
-...
-st.write("Instance Segmentation")
-st.subheader("Image")
-image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
-
-if image_file is not None:
-
-    st.image(load_image(image_file),width=250)
-
-model_path = "model_final_instance_segmentation.pth"
-
-cfg = get_cfg()
-# Force model to operate within CPU, erase if CUDA compatible devices ara available
-if not torch.cuda.is_available():
-    cfg.MODEL.DEVICE='cpu'
-# Add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-# Set threshold for this model
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  
-# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-cfg.MODEL.WEIGHTS = model_path
-# Initialize prediction model
+#detector.onVideo("pexels-tima-miroshnichenko-6388396.mp4")
+#@st.cache
+def func_1(x):
+    detector = Detector(model_type=x)
+    image_file = st.file_uploader("Upload An Image",type=['png','jpeg','jpg'])
+    if image_file is not None:
+        file_details = {"FileName":image_file.name,"FileType":image_file.type}
+        st.write(file_details)
+        img = Image.open(image_file)
+        st.image(img, caption='Uploaded Image.')
+        with open(image_file.name,mode = "wb") as f:
+            f.write(image_file.getbuffer())
+        st.success("Saved File")
+        detector.onImage(image_file.name)
+        img_ = Image.open("result.jpg")
+        st.image(img_, caption='Proccesed Image.')
 
 
 
-from detectron2.data.datasets import register_coco_instances
-register_coco_instances(f"Helmet_train1",{},  f"Train/_annotations.coco.json", f"Train")
-Helmet_metadata = MetadataCatalog.get("Helmet_train1")
-Helmet_metadata.thing_classes = ["Helmet"]
+def main():
+    with st.expander("About the App"):
+        st.markdown( '<p style="font-size: 30px;"><strong>Welcome to my Instance Segmentation App!</strong></p>', unsafe_allow_html= True)
+        st.markdown('<p style = "font-size : 20px; color : white;">This app was built using Streamlit, Detectron2 and OpenCv to demonstrate <strong>Instance Segmentation</strong> in both videos (pre-recorded) and images.</p>', unsafe_allow_html=True)
 
 
-predictor = DefaultPredictor(cfg)
 
-def inference(image):
-    #im = cv2.imread(image)
-    height = image.height
+    option = st.selectbox(
+     'What Type of File do you want to work with?',
+     ('Images', 'Videos'))
 
-    img = np.array(image.resize((800, height)))
-    outputs = predictor(img) 
-    v = Visualizer(img[:, :, ::-1],
-                   metadata= Helmet_metadata, 
-                   scale=0.5,
-                   instance_mode=ColorMode.IMAGE_BW  
-    )
-    #v = Visualizer(img, my_metadata, scale=1.2)
-    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        
-    return out.get_image()
+    #st.write('You selected:', option)
+    if option == "Images":
+        st.title('Instance Segmentation for Images')
+        st.subheader("""
+    This takes in an image and outputs the image ,providing the exact outline of objects within the image.
+    """)
+        func_1('instanceSegmentation')
 
-if st.button('Click for detection'):
-  inference(image_file)
-  st.image(processed_img, caption='Processed Image', use_column_width=True)
+if __name__ == '__main__':
+                main()
